8f6e09d0ce16e6c1619193fb0a84c1d324a63570
kashyapdhruv498 <kashyapdhruv498@gmail.com>
2023-08-15 12:28:39 +0530

    calc done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,97 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Calculator';
+
+  calValue: number = 0;
+  FuncT: any = 'NaN';
+
+  onClick(val: string, type: any) {
+    if (type == 'number') {
+      this.onClicknumber(val);
+    } else if (type == 'function') {
+      this.onClickfunction(val);
+    }
+  }
+
+  calnumber: string = 'noValue';
+
+  onClicknumber(val: string) {
+    if (this.calnumber !== 'noValue') {
+      this.calnumber = this.calnumber + val;
+    } else {
+      this.calnumber = val;
+    }
+
+    this.calValue = parseFloat(this.calnumber);
+  }
+
+  firstnumber: number = 0;
+  secondnumber: number = 0;
+
+  onClickfunction(val: string) {
+    if (val == 'c') {
+      this.clearAll();
+    } else if (this.FuncT == 'NaN') {
+      this.firstnumber = this.calValue;
+      this.calValue = 0;
+      this.calnumber = 'noValue';
+      this.FuncT = val;
+    } else if (this.FuncT !== 'NaN') {
+      this.secondnumber = this.calValue;
+      this.calculateValue(val);
+    }
+  }
+
+  calculateValue(val: string) {
+    if (this.FuncT == '+') {
+      const Total = this.firstnumber + this.secondnumber;
+      this.totalValue(Total, val);
+    }
+    if (this.FuncT == '-') {
+      const Total = this.firstnumber - this.secondnumber;
+      this.totalValue(Total, val);
+    }
+    if (this.FuncT == '*') {
+      const Total = this.firstnumber * this.secondnumber;
+      this.totalValue(Total, val);
+    }
+    if (this.FuncT == '/') {
+      const Total = this.firstnumber / this.secondnumber;
+      this.totalValue(Total, val);
+    }
+    if (this.FuncT == '%') {
+      const Total = this.firstnumber % this.secondnumber;
+      this.totalValue(Total, val);
+    }
+  }
+
+  totalValue(Total: number, val: string) {
+    this.calValue = Total;
+    this.firstnumber = Total;
+    this.secondnumber = 0;
+    this.calnumber = 'noValue';
+    this.FuncT = val;
+    if (val == '=') {
+      this.onEqualpress();
+    }
+  }
+
+  onEqualpress() {
+    this.firstnumber = 0;
+    this.secondnumber = 0;
+    this.FuncT = 'NaN';
+    this.calnumber = 'noValue';
+  }
+
+  clearAll() {
+    this.firstnumber = 0;
+    this.secondnumber = 0;
+    this.calValue = 0;
+    this.FuncT = 'NaN';
+    this.calnumber = 'noValue';
+  }
 }
